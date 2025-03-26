cee59cf9ba0d4c51cb1f065bc0dfdc5457730230
Frankllin151 <frankllinsilva300@gmail.com>
2025-03-26 14:19:16 -0300

    feat

--- resources/js/Pages/KnowBot.jsx
+++ resources/js/Pages/KnowBot.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
-import { usePage } from '@inertiajs/react';
+import { Head, usePage } from '@inertiajs/react';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 
 const KnowBot = () => {
   const page = usePage();
   console.log('usePage data:', page);
 
   return (
-    <div>
+   <AuthenticatedLayout
+   header={
+    <h2 className="text-xl font-semibold leading-tight text-gray-800">
+                   Adicionar Conhecimento
+                </h2>
+   }
+   >
+    <Head title='Adicionar conhecimento'/>
+     <div>
       <h1>KnowBot Page</h1>
       <p>Check the console to see the data returned by usePage.</p>
     </div>
+   </AuthenticatedLayout>
   );
 };
 
